a34cfac7a715b261bcbf6e33a2cfce271f75620c
Rajesh <Rajesh@RAJESH-PC>
2017-02-20 23:50:29 -0600

    Add project files.

---
+++ ConsoleApplication7/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceApplication
+{
+    public interface ITransactions
+    {
+        void showTransaction();
+        double getAmount();
+    }
+
+    public class Transaction : ITransactions
+    {
+        private string tCode;
+        private string date;
+        private double amount;
+        public Transaction()
+        {
+            tCode = " ";
+            date = " ";
+            amount = 0.0;
+        }
+
+        public Transaction(string c, string d, double a)
+        {
+            tCode = c;
+            date = d;
+            amount = a;
+        }
+
+        public double getAmount()
+        {
+            return amount;
+        }
+
+        public void showTransaction()
+        {
+            Console.WriteLine("Transaction: {0}", tCode);
+            Console.WriteLine("Date: {0}", date);
+            Console.WriteLine("Amount: {0}", getAmount());
+        }
+    }
+    class Tester
+    {
+        static void Main(string[] args)
+        {
+            Transaction t1 = new Transaction("1", "01/01/2017", 24.00);
+            Transaction t2 = new Transaction("2", "02/02/2017", 45.00);
+            t1.showTransaction();
+            t2.showTransaction();
+            Console.ReadKey();
+        }
+    }
+}
